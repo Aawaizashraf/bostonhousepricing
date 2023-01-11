e46c071eebfbf3ea756aff44b48bb5f5ff566244
Syed Aawaiz Ashraf <syedaawaiz49@gmail.com>
2023-01-11 18:29:20 +0530

    Host and port number added in app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict():
     return render_template("home.html",prediction_text="The House Price Prediction is {}".format(output))
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
